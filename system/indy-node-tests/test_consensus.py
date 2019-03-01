44c4022eec977163cf27837cb66fcfddc84ee718
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-03-01 17:18:01 +0300

    check return code for testinfra calls
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/indy-node-tests/test_consensus.py
+++ system/indy-node-tests/test_consensus.py
@@ -22,30 +22,32 @@ async def test_consensus_restore_after_f_plus_one(pool_handler, wallet_handler,
     # 7/7 online - can w+r
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did1)
     # 5/7 online - can w+r
-    outputs = [host.run('systemctl stop indy-node') for host in hosts[-2:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl stop indy-node') for host in hosts[-2:]]
+    # print(outputs)
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did2)
     # 4/7 online - can r only
-    hosts[4].run('systemctl stop indy-node')
+    output = hosts[4].check_output('systemctl stop indy-node')
+    # print(output)
     time.sleep(60)
     with pytest.raises(IndyError, match='Consensus is impossible'):
         await nym_helper(pool_handler, wallet_handler, trustee_did, did3, None, None, None)
     res1 = await get_nym_helper(pool_handler, wallet_handler, trustee_did, did1)
     assert res1['result']['seqNo'] is not None
     # 3/7 online - can r only
-    hosts[3].run('systemctl stop indy-node')
+    output = hosts[3].check_output('systemctl stop indy-node')
+    # print(output)
     with pytest.raises(IndyError, match='Consensus is impossible'):
         await nym_helper(pool_handler, wallet_handler, trustee_did, did4, None, None, None)
     res2 = await get_nym_helper(pool_handler, wallet_handler, trustee_did, did2)
     assert res2['result']['seqNo'] is not None
     # 5/7 online - can w+r
-    outputs = [host.run('systemctl start indy-node') for host in hosts[3:5]]
-    print(outputs)
+    outputs = [host.check_output('systemctl start indy-node') for host in hosts[3:5]]
+    # print(outputs)
     time.sleep(60)
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did3)
     # 7/7 online - can w+r
-    outputs = [host.run('systemctl start indy-node') for host in hosts[-2:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl start indy-node') for host in hosts[-2:]]
+    # print(outputs)
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did4)
 
 
@@ -60,16 +62,16 @@ async def test_consensus_state_proof_reading(pool_handler, wallet_handler,
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did1)
     time.sleep(15)
     # Stop all except 1
-    outputs = [host.run('systemctl stop indy-node') for host in hosts[1:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl stop indy-node') for host in hosts[1:]]
+    # print(outputs)
     time.sleep(15)
     res = await get_nym_helper(pool_handler, wallet_handler, trustee_did, did1)
     assert res['result']['seqNo'] is not None
     # Stop the last one
-    hosts[0].run('systemctl stop indy-node')
+    hosts[0].check_output('systemctl stop indy-node')
     # Start all
-    outputs = [host.run('systemctl start indy-node') for host in hosts]
-    print(outputs)
+    outputs = [host.check_output('systemctl start indy-node') for host in hosts]
+    # print(outputs)
     time.sleep(45)
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did2)
 
@@ -85,24 +87,24 @@ async def test_consensus_n_and_f_changing(pool_handler, wallet_handler, get_defa
     alias, target_did = await demote_random_node(pool_handler, wallet_handler, trustee_did)
     temp_hosts = hosts.copy()
     temp_hosts.pop(int(alias[4:])-1)
-    outputs = [host.run('systemctl stop indy-node') for host in temp_hosts[-2:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl stop indy-node') for host in temp_hosts[-2:]]
+    # print(outputs)
     time.sleep(60)
     with pytest.raises(IndyError, match='Consensus is impossible'):
         await nym_helper(pool_handler, wallet_handler, trustee_did, did1, None, None, None)
-    outputs = [host.run('systemctl start indy-node') for host in temp_hosts[-2:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl start indy-node') for host in temp_hosts[-2:]]
+    # print(outputs)
     time.sleep(60)
     await promote_node(pool_handler, wallet_handler, trustee_did, alias, target_did)
     time.sleep(60)
-    outputs = [host.run('systemctl stop indy-node') for host in hosts[-2:]]
-    print(outputs)
+    outputs = [host.check_output('systemctl stop indy-node') for host in hosts[-2:]]
+    # print(outputs)
     res2 = await nym_helper(pool_handler, wallet_handler, trustee_did, did2, None, None, None)
     assert res2['op'] == 'REPLY'
-    hosts[0].run('systemctl stop indy-node')
+    hosts[0].check_output('systemctl stop indy-node')
     time.sleep(60)
     with pytest.raises(IndyError, match='Consensus is impossible'):
         await nym_helper(pool_handler, wallet_handler, trustee_did, did3, None, None, None)
     # Start all
-    outputs = [host.run('systemctl start indy-node') for host in hosts]
-    print(outputs)
+    outputs = [host.check_output('systemctl start indy-node') for host in hosts]
+    # print(outputs)
--- system/indy-node-tests/test_vc.py
+++ system/indy-node-tests/test_vc.py
@@ -11,8 +11,8 @@ async def test_vc_by_restart(pool_handler, wallet_handler, get_default_trustee):
     primary_before, _, _ = await get_primary(pool_handler, wallet_handler, trustee_did)
     print('\nPrimary before: {}'.format(primary_before))
     host = testinfra.get_host('docker://node'+primary_before)
-    output = host.run('systemctl stop indy-node')
-    print(output)
+    output = host.check_output('systemctl stop indy-node')
+    # print(output)
     primary_after = primary_before
     while primary_before == primary_after:
         primary_after, _, _ = await get_primary(pool_handler, wallet_handler, trustee_did)
@@ -21,8 +21,8 @@ async def test_vc_by_restart(pool_handler, wallet_handler, get_default_trustee):
     assert primary_before != primary_after
     await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did2)
     host = testinfra.get_host('docker://node'+primary_before)
-    output = host.run('systemctl start indy-node')
-    print(output)
+    output = host.check_output('systemctl start indy-node')
+    # print(output)
     time.sleep(30)
     check_ledger_sync()
 
