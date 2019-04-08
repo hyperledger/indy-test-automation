8260b7065bc36637a4baab4663670663eac5ddc4
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-04-08 19:01:17 +0300

    update
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/TestAuditSuite.py
+++ system/TestAuditSuite.py
@@ -6,7 +6,7 @@ from system.utils import *
 class TestAuditSuite:
 
     @pytest.mark.asyncio
-    async def test_case_1(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_restart_one_node(self, pool_handler, wallet_handler, get_default_trustee):
         trustee_did, _ = get_default_trustee
         hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         for i in range(15):
@@ -47,10 +47,11 @@ class TestAuditSuite:
         check_ledger_sync()
         await send_and_get_nym(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0])
 
+    @pytest.mark.parametrize('node_num_shift', [0, 1, 5])
     @pytest.mark.asyncio
-    async def test_case_2(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_restart_master_backup_non_primary(self, pool_handler, wallet_handler, get_default_trustee,
+                                                          node_num_shift):
         trustee_did, _ = get_default_trustee
-        hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         primary1, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
         output = testinfra.get_host('ssh://node{}'.format(primary1)).check_output('systemctl stop indy-node')
         print(output)
@@ -59,8 +60,11 @@ class TestAuditSuite:
             await nym_helper(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0], None, None, None)
         output = testinfra.get_host('ssh://node{}'.format(primary1)).check_output('systemctl start indy-node')
         print(output)
-        output = hosts[1].check_output('systemctl restart indy-node')
+        primary2, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
+        output = testinfra.get_host('ssh://node{}'.format(int(primary2)+node_num_shift))\
+            .check_output('systemctl restart indy-node')
         print(output)
+        time.sleep(60)
         for i in range(30):
             await nym_helper(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0], None, None, None)
         time.sleep(30)
@@ -68,7 +72,7 @@ class TestAuditSuite:
         await send_and_get_nym(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0])
 
     @pytest.mark.asyncio
-    async def test_case_3(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_restart_all_nodes_at_the_same_time(self, pool_handler, wallet_handler, get_default_trustee):
         trustee_did, _ = get_default_trustee
         hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         primary1, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
@@ -89,7 +93,7 @@ class TestAuditSuite:
         await send_and_get_nym(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0])
 
     @pytest.mark.asyncio
-    async def test_case_4(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_restart_f_nodes(self, pool_handler, wallet_handler, get_default_trustee):
         trustee_did, _ = get_default_trustee
         hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         primary1, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
@@ -110,7 +114,7 @@ class TestAuditSuite:
         await send_and_get_nym(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0])
 
     @pytest.mark.asyncio
-    async def test_case_5(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_5_restart_n_minus_f_minus_one_nodes(self, pool_handler, wallet_handler, get_default_trustee):
         trustee_did, _ = get_default_trustee
         hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         primary1, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
@@ -131,7 +135,7 @@ class TestAuditSuite:
         await send_and_get_nym(pool_handler, wallet_handler, trustee_did, random_did_and_json()[0])
 
     @pytest.mark.asyncio
-    async def test_case_6(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_restart_all_nodes_one_by_one(self, pool_handler, wallet_handler, get_default_trustee):
         trustee_did, _ = get_default_trustee
         hosts = [testinfra.get_host('ssh://node{}'.format(i)) for i in range(1, 8)]
         primary1, alias, target_did = await get_primary(pool_handler, wallet_handler, trustee_did)
