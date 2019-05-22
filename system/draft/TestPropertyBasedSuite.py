223a3f70bf29c4abe51f3d2b3bcda90f4ca9f63c
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2019-05-22 11:38:26 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- system/draft/TestPropertyBasedSuite.py
+++ system/draft/TestPropertyBasedSuite.py
@@ -9,6 +9,7 @@ import copy
 @pytest.mark.usefixtures('docker_setup_and_teardown')
 class TestPropertyBasedSuite:
 
+    @pytest.mark.skip('example')
     @settings(deadline=None, max_examples=100)
     @given(var_bin=strategies.binary(5, 25).filter(lambda x: x != b'\x00\x00\x00\x00\x00'),  # <<< filter
            var_char=strategies.characters('S').filter(lambda x: x not in ['@', '#', '$']),  # <<< filter
@@ -31,12 +32,12 @@ class TestPropertyBasedSuite:
         print('-'*25)
 
     @settings(deadline=None, max_examples=100)
-    @given(reqid=strategies.integers(min_value=1),
+    @given(reqid=strategies.integers(min_value=1, max_value=999999999999999),
            dest=strategies.text(ascii_letters, min_size=16, max_size=16),
-           verkey=strategies.text(ascii_letters, min_size=32, max_size=32),
+           # verkey=strategies.text(ascii_letters, min_size=32, max_size=32),
            alias=strategies.text(min_size=1, max_size=10000))
     @pytest.mark.asyncio
-    async def test_case_nym(self, pool_handler, wallet_handler, get_default_trustee, reqid, dest, verkey, alias):
+    async def test_case_nym(self, pool_handler, wallet_handler, get_default_trustee, reqid, dest, alias):
         trustee_did, trustee_vk = get_default_trustee
         roles = ['0', '2', '101', '201']
         req = {'protocolVersion': 2,
@@ -45,7 +46,7 @@ class TestPropertyBasedSuite:
                'operation':
                    {'type': '1',
                     'dest': base58.b58encode(dest).decode(),
-                    'verkey': base58.b58encode(verkey).decode(),
+                    # 'verkey': base58.b58encode(verkey).decode(),
                     'role': random.choice(roles),
                     'alias': alias
                     }}
@@ -55,8 +56,14 @@ class TestPropertyBasedSuite:
         print(res)
         assert res['op'] == 'REPLY'
 
+    @settings(deadline=None, max_examples=100)
+    @given(reqid=strategies.integers(min_value=1, max_value=999999999999999),
+           xhash=strategies.text().map(lambda x: hashlib.sha256(x.encode()).hexdigest()),
+           key=strategies.text(printable),
+           value=strategies.text(printable),
+           enc=strategies.text(min_size=1))
     @pytest.mark.asyncio
-    async def test_case_attrib(self, pool_handler, wallet_handler, get_default_trustee):
+    async def test_case_attrib(self, pool_handler, wallet_handler, get_default_trustee, reqid, xhash, key, value, enc):
         trustee_did, trustee_vk = get_default_trustee
         target_did, target_vk = await did.create_and_store_my_did(wallet_handler, '{}')
         res = await send_nym(pool_handler, wallet_handler, trustee_did, target_did, target_vk)
@@ -69,8 +76,8 @@ class TestPropertyBasedSuite:
                          }}
 
         req1 = copy.deepcopy(req_base)
-        req1['reqId'] = int(time.time())
-        req1['operation']['hash'] = hashlib.sha256().hexdigest()
+        req1['reqId'] = reqid + 1
+        req1['operation']['hash'] = xhash
         res1 = json.loads\
             (await ledger.sign_and_submit_request(pool_handler, wallet_handler, target_did, json.dumps(req1)))
         print(req1)
@@ -78,8 +85,8 @@ class TestPropertyBasedSuite:
         assert res1['op'] == 'REPLY'
 
         req2 = copy.deepcopy(req_base)
-        req2['reqId'] = int(time.time())
-        req2['operation']['raw'] = '{"key": "value"}'
+        req2['reqId'] = reqid + 2
+        req2['operation']['raw'] = json.dumps({key: value})
         res2 = json.loads\
             (await ledger.sign_and_submit_request(pool_handler, wallet_handler, target_did, json.dumps(req2)))
         print(req2)
@@ -87,10 +94,38 @@ class TestPropertyBasedSuite:
         assert res2['op'] == 'REPLY'
 
         req3 = copy.deepcopy(req_base)
-        req3['reqId'] = int(time.time())
-        req3['operation']['enc'] = random_string(10)
+        req3['reqId'] = reqid + 3
+        req3['operation']['enc'] = enc
         res3 = json.loads\
             (await ledger.sign_and_submit_request(pool_handler, wallet_handler, target_did, json.dumps(req3)))
         print(req3)
         print(res3)
         assert res3['op'] == 'REPLY'
+
+    @settings(deadline=None, max_examples=100)
+    @given(reqid=strategies.integers(min_value=1, max_value=999999999999999),
+           version=strategies.floats(min_value=0.1, max_value=999.999),
+           name=strategies.text(min_size=1),
+           attrs=strategies.lists(strategies.text(min_size=1), min_size=1, max_size=125))
+    @pytest.mark.asyncio
+    async def test_case_schema(self, pool_handler, wallet_handler, get_default_trustee, reqid, version, name, attrs):
+        trustee_did, trustee_vk = get_default_trustee
+        creator_did, creator_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, creator_did, creator_vk, None, 'TRUSTEE')
+        assert res['op'] == 'REPLY'
+        req = {'protocolVersion': 2,
+               'reqId': reqid,
+               'identifier': creator_did,
+               'operation':
+               {'type': '101',
+                'data': {
+                    'version': str(version),
+                    'name': name,
+                    'attr_names': attrs
+                    }
+               }}
+        res = json.loads\
+            (await ledger.sign_and_submit_request(pool_handler, wallet_handler, creator_did, json.dumps(req)))
+        print(req)
+        print(res)
+        assert res['op'] == 'REPLY'
