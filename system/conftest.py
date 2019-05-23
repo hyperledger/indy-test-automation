aa078d0d59f2ddc21429ebe947f978650244f341
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-05-23 18:57:43 +0300

    update and add fixtures to set fees and mint tokens
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/conftest.py
+++ system/conftest.py
@@ -59,6 +59,37 @@ async def initial_token_minting(pool_handler, wallet_handler, get_default_truste
     await yield_(address)
 
 
+@pytest.fixture()
+@async_generator
+async def initial_fees_setting(pool_handler, wallet_handler, get_default_trustee):
+    await payment_initializer('libsovtoken.so', 'sovtoken_init')
+    libsovtoken_payment_method = 'sov'
+    trustee_did, _ = get_default_trustee
+    trustee_did_second, trustee_vk_second = await did.create_and_store_my_did(wallet_handler, json.dumps({}))
+    trustee_did_third, trustee_vk_third = await did.create_and_store_my_did(wallet_handler, json.dumps({}))
+    await send_nym(pool_handler, wallet_handler, trustee_did, trustee_did_second, trustee_vk_second, None, 'TRUSTEE')
+    await send_nym(pool_handler, wallet_handler, trustee_did, trustee_did_third, trustee_vk_third, None, 'TRUSTEE')
+    fees = {'trustee_0': 0 * 100000,
+            'steward_0': 0 * 100000,
+            'trust_anchor_0': 0 * 100000,
+            'network_monitor_0': 0 * 100000,
+            'add_identity_owner_50': 50 * 100000,
+            'edit_identity_owner_0': 0 * 100000,
+            'add_schema_250': 250 * 100000,
+            'add_cred_def_125': 125 * 100000,
+            'add_rrd_100': 100 * 100000,
+            'add_rre_0_5': int(0.5 * 100000)
+            }
+    req = await payment.build_set_txn_fees_req(wallet_handler, trustee_did, libsovtoken_payment_method,
+                                               json.dumps(fees))
+    req = await ledger.multi_sign_request(wallet_handler, trustee_did, req)
+    req = await ledger.multi_sign_request(wallet_handler, trustee_did_second, req)
+    req = await ledger.multi_sign_request(wallet_handler, trustee_did_third, req)
+    res = json.loads(await ledger.submit_request(pool_handler, req))
+    assert res['op'] == 'REPLY'
+    await yield_(fees)
+
+
 @pytest.fixture(scope='function')
 @async_generator
 async def docker_setup_and_teardown():
