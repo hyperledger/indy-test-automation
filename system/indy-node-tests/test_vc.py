217d233ac192e4824f1da2ed2187d702b6b2b115
Andrey Kononykhin <andkononykhin@gmail.com>
2019-06-06 14:52:51 +0300

    updates tests
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- system/indy-node-tests/test_vc.py
+++ system/indy-node-tests/test_vc.py
@@ -58,18 +58,15 @@ async def test_vc_by_demotion(pool_handler, wallet_handler, get_default_trustee)
 async def test_demotion_of_backup_primary_with_restart_with_vc(
     pool_handler, wallet_handler, get_default_trustee, nodes_num, check_no_failures_fixture
 ):
-    logger.info("1 Have 8 nodes in the pool, so that primaries are [Node1, Node2, Node3]")
-
     R0_PRIMARY_ID = 1
     R1_PRIMARY_ID = 2
     R2_PRIMARY_ID = 3
-    hosts = [NodeHost(node_id + 1) for node_id in range(nodes_num)]
 
+    hosts = [NodeHost(node_id + 1) for node_id in range(nodes_num)]
     trustee_did, _ = get_default_trustee
-    did1, _ = await did.create_and_store_my_did(wallet_handler, '{}')
-    did2, _ = await did.create_and_store_my_did(wallet_handler, '{}')
-    did3, _ = await did.create_and_store_my_did(wallet_handler, '{}')
-    await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did1)
+
+    logger.info("1 Have 8 nodes in the pool, so that primaries are [Node1, Node2, Node3]")
+    await check_pool_performs_write_read(pool_handler, wallet_handler, trustee_did)
 
     pool_info = get_pool_info(str(R0_PRIMARY_ID))
 
@@ -82,42 +79,27 @@ async def test_demotion_of_backup_primary_with_restart_with_vc(
     )
 
     logger.info("3 Wait for view change")
-
-    # timeouts
+    # TODO timeouts
     logger.info('Primary before: {}'.format(R0_PRIMARY_ID))
-    primary_after = await wait_until_vc_is_done(
-        str(R0_PRIMARY_ID), pool_handler, wallet_handler, trustee_did, cycles_limit=5, sleep=5
+    primary_after = await ensure_primary_changed(
+        pool_handler, wallet_handler, trustee_did, str(R0_PRIMARY_ID)
     )
     logger.info('Primary after: {}'.format(primary_after))
 
     logger.info("4 Order 1 more txn")
-
-    await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did2)
+    await ensure_pool_is_workable(pool_handler, wallet_handler, trustee_did)
 
     logger.info("5 Restart the whole pool")
-
-    # restart all nodes using stop/start just to avoid
-    # the case when some node is already restarted while
-    # some others are still running
-    # TODO use threads to make that concurrent/async
-    for host in hosts:
-        host.stop_service()
-    for host in hosts:
-        host.start_service()
+    restart_pool(hosts)
 
     logger.info("6 Make sure that the pool restarted correctly, and can order txns")
 
-    logger.info("5.1 ensure that pool is in sync")
-
+    logger.info("6.1 ensure that pool is in sync")
     # TODO timeouts
-    await eventually_positive(
-        check_ledger_sync, node_ids=[h.id for h in hosts if h.id != R2_PRIMARY_ID],
-        cycles_limit=10,
-        sleep=5
-    )
-    logger.info("5.2 ensure that pool orders requests")
+    await ensure_pool_is_in_sync(node_ids=[h.id for h in hosts if h.id != R2_PRIMARY_ID])
 
-    await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did3)
+    logger.info("6.2 ensure that pool orders requests")
+    await ensure_pool_is_workable(pool_handler, wallet_handler, trustee_did)
 
 
 @pytest.mark.nodes_num(8)
@@ -128,24 +110,19 @@ async def test_demotion_of_backup_primary_with_restart_without_vc(
     R0_PRIMARY_ID = 1
     R1_PRIMARY_ID = 2
     R2_PRIMARY_ID = 3
-    logger.info("1 Have 8 nodes in the pool, so that primaries are [Node1, Node2, Node3]")
 
     hosts = [NodeHost(node_id + 1) for node_id in range(nodes_num)]
-
     trustee_did, _ = get_default_trustee
-    did1, _ = await did.create_and_store_my_did(wallet_handler, '{}')
-    did2, _ = await did.create_and_store_my_did(wallet_handler, '{}')
-    await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did1)
 
-    pool_info = get_pool_info(str(R0_PRIMARY_ID))
+    logger.info("1 Have 8 nodes in the pool, so that primaries are [Node1, Node2, Node3]")
+    await check_pool_performs_write_read(pool_handler, wallet_handler, trustee_did)
 
     logger.info("2 Stop Node2 to delay the view change to viewNo=1")
-
-    host2 = hosts.pop(R1_PRIMARY_ID - 1)
+    pool_info = get_pool_info(str(R0_PRIMARY_ID))
+    host2 = hosts[R1_PRIMARY_ID - 1]
     host2.stop_service()
 
     logger.info("3 Demote Node3: it will trigger a view change to viewNo=1")
-
     # which in turn will trigger a view change timeout since Node2 (the next primary)
     # has been stopped
     primary_r2_alias = get_node_alias(R2_PRIMARY_ID)
@@ -155,30 +132,12 @@ async def test_demotion_of_backup_primary_with_restart_without_vc(
     )
 
     logger.info("4 Restart the whole pool right after Demote NODE txn is written on all nodes")
-
-    # restart all nodes
-    # using stop/start just to avoid the case when some node
-    # is already restarted while some others still running
-    # TODO
-    # - use threads to make that async
-    # - move to helper
-    for host in hosts:
-        host.stop_service()
-    for host in hosts:
-        host.start_service()
+    restart_pool(hosts)
 
     logger.info("5 Make sure that the pool restarted correctly, and can order txns")
-
     logger.info("5.1 ensure that pool is in sync")
-
     # TODO timeouts
-    await eventually_positive(
-        check_ledger_sync,
-        node_ids=[h.id for h in hosts if h.id not in (R1_PRIMARY_ID, R2_PRIMARY_ID)],
-        cycles_limit=10,
-        sleep=5
-    )
+    await ensure_pool_is_in_sync(node_ids=[h.id for h in hosts if h.id != R2_PRIMARY_ID])
 
     logger.info("5.2 ensure that pool orders requests")
-
-    await send_and_get_nym(pool_handler, wallet_handler, trustee_did, did2)
+    await ensure_pool_is_workable(pool_handler, wallet_handler, trustee_did, timeout=60)
