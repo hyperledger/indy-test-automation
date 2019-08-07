6532cf42c9c9a530eee901568427dddcf9a6e175
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-08-07 12:02:59 +0300

    update
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/indy-node-tests/TestEndorserSuite.py
+++ system/indy-node-tests/TestEndorserSuite.py
@@ -9,22 +9,21 @@ logger = logging.getLogger(__name__)
 @pytest.mark.usefixtures('docker_setup_and_teardown')
 class TestEndorserSuite:
 
-    @pytest.mark.parametrize('role', ['TRUSTEE', 'STEWARD', 'ENDORSER'])
+    @pytest.mark.parametrize('role', [None, 'NETWORK_MONITOR', 'STEWARD', 'TRUSTEE'])
     @pytest.mark.asyncio
     async def test_case_endorser_roles(
             self, pool_handler, wallet_handler, get_default_trustee, role
     ):
-        # acceptable roles are ENDORSER, STEWARD, TRUSTEE
         trustee_did, _ = get_default_trustee
         test_did, test_vk = await did.create_and_store_my_did(wallet_handler, '{}')
         none_did, none_vk = await did.create_and_store_my_did(wallet_handler, '{}')
         e_did, e_vk = await did.create_and_store_my_did(wallet_handler, '{}')
-        res = await send_nym(pool_handler, wallet_handler, trustee_did, none_did, none_vk, 'No role', None)
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, none_did, none_vk, 'Not endorser', role)
         assert res['op'] == 'REPLY'
-        res = await send_nym(pool_handler, wallet_handler, trustee_did, e_did, e_vk, 'Endorser', role)
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, e_did, e_vk, 'Endorser', 'ENDORSER')
         assert res['op'] == 'REPLY'
 
-        # negative case - build txn with endorser, append none role did as endorser, multisign with both
+        # negative case - build txn with endorser, append wrong role did as endorser, multisign with both
         req0 = await ledger.build_nym_request(e_did, test_did, test_vk, 'Alias', None)
         req0 = await ledger.append_request_endorser(req0, none_did)
         req0 = await ledger.multi_sign_request(wallet_handler, e_did, req0)
@@ -33,7 +32,7 @@ class TestEndorserSuite:
         print(res0)
         assert res0['op'] == 'REJECT'
 
-        # positive case - build txn with none role did, append endorser as endorser, multisign with both
+        # positive case - build txn with any role did, append endorser as endorser, multisign with both
         req1 = await ledger.build_nym_request(none_did, test_did, test_vk, 'Alias', None)
         req1 = await ledger.append_request_endorser(req1, e_did)
         req1 = await ledger.multi_sign_request(wallet_handler, none_did, req1)
@@ -42,14 +41,45 @@ class TestEndorserSuite:
         print(res1)
         assert res1['op'] == 'REPLY'
 
+    @pytest.mark.parametrize('role', [None, 'NETWORK_MONITOR'])
     @pytest.mark.asyncio
     async def test_case_endorser_specification(
-            self, pool_handler, wallet_handler, get_default_trustee
+            self, pool_handler, wallet_handler, get_default_trustee, role
     ):
-        pass
+        trustee_did, _ = get_default_trustee
+        test_did, test_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        some_low_role_did, some_low_role_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        e_did, e_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        res = await send_nym(
+            pool_handler, wallet_handler, trustee_did, some_low_role_did, some_low_role_vk, 'Not endorser', role
+        )
+        assert res['op'] == 'REPLY'
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, e_did, e_vk, 'Endorser', 'ENDORSER')
+        assert res['op'] == 'REPLY'
+
+        # build nym and DO NOT append endorser
+        req = await ledger.build_nym_request(some_low_role_did, test_did, test_vk, 'Alias', None)
+        # but sign with two signatures
+        req = await ledger.multi_sign_request(wallet_handler, some_low_role_did, req)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REJECT'
+
+        # build schema and DO NOT append endorser
+        schema_id, schema_json = await anoncreds.issuer_create_schema(
+            some_low_role_did, 'Schema 9', '9.9', json.dumps(['name', 'surname'])
+        )
+        req = await ledger.build_schema_request(some_low_role_did, schema_json)
+        # but sign with two signatures
+        req = await ledger.multi_sign_request(wallet_handler, some_low_role_did, req)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REJECT'
 
     @pytest.mark.asyncio
-    async def test_case_happy_path(
+    async def test_case_full_path(
             self, pool_handler, wallet_handler, get_default_trustee
     ):
         trustee_did, _ = get_default_trustee
@@ -61,12 +91,22 @@ class TestEndorserSuite:
         res = await send_nym(pool_handler, wallet_handler, trustee_did, e_did, e_vk, 'Endorser', 'ENDORSER')
         assert res['op'] == 'REPLY'
 
-        # try to add new did by none role did - should fail
+        # sign nym by author only
         req = await ledger.build_nym_request(off_did, test_did, test_vk, 'Alias 1', None)
-        res = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, off_did, req))
-        assert res['op'] == 'REJECT'
-        # add new did using none role did as builder and endorser did as endorser
-        req0 = await ledger.build_nym_request(off_did, test_did, test_vk, 'Alias 1', None)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, off_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # sign nym  by endorser only
+        req = await ledger.build_nym_request(off_did, test_did, test_vk, 'Alias 2', None)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # add new did using none role did as author and endorser did as endorser
+        req0 = await ledger.build_nym_request(off_did, test_did, test_vk, 'Alias 3', None)
         req0 = await ledger.append_request_endorser(req0, e_did)
         req0 = await ledger.multi_sign_request(wallet_handler, off_did, req0)
         req0 = await ledger.multi_sign_request(wallet_handler, e_did, req0)
@@ -77,10 +117,20 @@ class TestEndorserSuite:
         schema_id, schema_json = await anoncreds.issuer_create_schema(
             off_did, 'Schema 1', '0.1', json.dumps(['a1', 'a2'])
         )
-        # try to add new schema by none role did - should fail
+        # sign schema by author only
         req = await ledger.build_schema_request(off_did, schema_json)
-        res = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, off_did, req))
-        assert res['op'] == 'REJECT'
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, off_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # sign schema by endorser only
+        req = await ledger.build_schema_request(off_did, schema_json)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
         # add new schema using none role did as builder and endorser did as endorser
         req1 = await ledger.build_schema_request(off_did, schema_json)
         req1 = await ledger.append_request_endorser(req1, e_did)
@@ -96,10 +146,20 @@ class TestEndorserSuite:
         cred_def_id, cred_def_json = await anoncreds.issuer_create_and_store_credential_def(
             wallet_handler, off_did, schema_json, 'cred def tag', None, json.dumps({'support_revocation': True})
         )
-        # try to add new cred def by none role did - should fail
+        # sign cred def by author only
         req = await ledger.build_cred_def_request(off_did, cred_def_json)
-        res = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, off_did, req))
-        assert res['op'] == 'REJECT'
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, off_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # sign cred def by endorser only
+        req = await ledger.build_cred_def_request(off_did, cred_def_json)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
         # add new cred def using none role did as builder and endorser did as endorser
         req2 = await ledger.build_cred_def_request(off_did, cred_def_json)
         req2 = await ledger.append_request_endorser(req2, e_did)
@@ -116,10 +176,20 @@ class TestEndorserSuite:
                 {'max_cred_num': 100, 'issuance_type': 'ISSUANCE_BY_DEFAULT'}
             ), tails_writer_handle
         )
-        # try to add new revoc reg def by none role did - should fail
+        # sign revoc reg def by author only
         req = await ledger.build_revoc_reg_def_request(off_did, revoc_reg_def_json)
-        res = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, off_did, req))
-        assert res['op'] == 'REJECT'
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, off_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # sign revoc reg def by endorser only
+        req = await ledger.build_revoc_reg_def_request(off_did, revoc_reg_def_json)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
         # add new revoc reg def using none role did as builder and endorser did as endorser
         req3 = await ledger.build_revoc_reg_def_request(off_did, revoc_reg_def_json)
         req3 = await ledger.append_request_endorser(req3, e_did)
@@ -129,10 +199,20 @@ class TestEndorserSuite:
         print(res3)
         assert res3['op'] == 'REPLY'
 
-        # try to add new revoc reg entry by none role did - should fail
+        # sign revoc reg entry by author only
         req = await ledger.build_revoc_reg_entry_request(off_did, revoc_reg_id, 'CL_ACCUM', revoc_reg_entry_json)
-        res = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, off_did, req))
-        assert res['op'] == 'REJECT'
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, off_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
+        # sign revoc reg entry by endorser only
+        req = await ledger.build_revoc_reg_entry_request(off_did, revoc_reg_id, 'CL_ACCUM', revoc_reg_entry_json)
+        req = await ledger.append_request_endorser(req, e_did)
+        req = await ledger.multi_sign_request(wallet_handler, e_did, req)
+        res = json.loads(await ledger.submit_request(pool_handler, req))
+        print(res)
+        assert res['op'] == 'REQNACK'
         # add new revoc reg entry using none role did as builder and endorser did as endorser
         req4 = await ledger.build_revoc_reg_entry_request(off_did, revoc_reg_id, 'CL_ACCUM', revoc_reg_entry_json)
         req4 = await ledger.append_request_endorser(req4, e_did)
