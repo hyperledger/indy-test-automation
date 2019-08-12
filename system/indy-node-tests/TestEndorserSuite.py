f5780fc235a6319727cbf9a94e5c8d4e906ddbb7
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-08-12 19:11:20 +0300

    update
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/indy-node-tests/TestEndorserSuite.py
+++ system/indy-node-tests/TestEndorserSuite.py
@@ -1,6 +1,7 @@
 import pytest
 import asyncio
 from system.utils import *
+from indy import payment
 
 import logging
 logger = logging.getLogger(__name__)
@@ -236,3 +237,94 @@ class TestEndorserSuite:
         res4 = json.loads(await ledger.submit_request(pool_handler, req4))
         print(res4)
         assert res4['op'] == 'REPLY'
+
+    @pytest.mark.asyncio
+    async def test_case_endorser_fees(
+            self, payment_init, pool_handler, wallet_handler, get_default_trustee,
+            initial_token_minting, initial_fees_setting
+    ):
+        libsovtoken_payment_method = 'sov'
+        trustee_did, _ = get_default_trustee
+        address = initial_token_minting
+        author_did, author_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        e_did, e_vk = await did.create_and_store_my_did(wallet_handler, '{}')
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, author_did, author_vk, 'No role', None)
+        assert res['op'] == 'REPLY'
+        res = await send_nym(pool_handler, wallet_handler, trustee_did, e_did, e_vk, 'Endorser', 'ENDORSER')
+        assert res['op'] == 'REPLY'
+
+        # set fees
+        print(initial_fees_setting)
+
+        # set auth rule for schema
+        req = await ledger.build_auth_rule_request(trustee_did, '101', 'ADD', '*', None, '*',
+                                                   json.dumps({
+                                                       'constraint_id': 'OR',
+                                                       'auth_constraints': [
+                                                           {
+                                                               'constraint_id': 'ROLE',
+                                                               'role': '0',
+                                                               'sig_count': 1,
+                                                               'need_to_be_owner': False,
+                                                               'metadata': {'fees': 'add_schema_250'}
+                                                           },
+                                                           {
+                                                               'constraint_id': 'ROLE',
+                                                               'role': '2',
+                                                               'sig_count': 1,
+                                                               'need_to_be_owner': False,
+                                                               'metadata': {'fees': 'add_schema_250'}
+                                                           },
+                                                           {
+                                                               'constraint_id': 'ROLE',
+                                                               'role': '101',
+                                                               'sig_count': 1,
+                                                               'need_to_be_owner': False,
+                                                               'metadata': {'fees': 'add_schema_250'}
+                                                           }
+                                                       ]
+                                                   }))
+        res1 = json.loads(await ledger.sign_and_submit_request(pool_handler, wallet_handler, trustee_did, req))
+        print(res1)
+        assert res1['op'] == 'REPLY'
+
+        # send schema with fees using endorser
+        req, _ = await payment.build_get_payment_sources_request(wallet_handler, author_did, address)
+        res = await ledger.sign_and_submit_request(pool_handler, wallet_handler, author_did, req)
+        source1 = json.loads(
+            await payment.parse_get_payment_sources_response(libsovtoken_payment_method, res)
+        )[0]['source']
+
+        # add endorser first
+        schema_id, schema_json = await anoncreds.issuer_create_schema(
+            author_did, random_string(5), '1.0', json.dumps(['name', 'age'])
+        )
+        req = await ledger.build_schema_request(author_did, schema_json)
+        req = await ledger.append_request_endorser(req, e_did)
+        req_with_fees_json, _ = await payment.add_request_fees(
+            wallet_handler, author_did, req, json.dumps([source1]), json.dumps(
+                [{'recipient': address, 'amount': 750 * 100000}]
+            ), None
+        )
+        req_with_fees_json = await ledger.multi_sign_request(wallet_handler, author_did, req_with_fees_json)
+        req_with_fees_json = await ledger.multi_sign_request(wallet_handler, e_did, req_with_fees_json)
+        res2 = json.loads(await ledger.submit_request(pool_handler, req_with_fees_json))
+        print(res2)
+        assert res2['op'] == 'REPLY'
+
+        # add fees first
+        schema_id, schema_json = await anoncreds.issuer_create_schema(
+            author_did, random_string(5), '0.1', json.dumps(['age', 'name'])
+        )
+        req = await ledger.build_schema_request(author_did, schema_json)
+        req_with_fees_json, _ = await payment.add_request_fees(
+            wallet_handler, author_did, req, json.dumps([source1]), json.dumps(
+                [{'recipient': address, 'amount': 500 * 100000}]
+            ), None
+        )
+        req_with_fees_json = await ledger.append_request_endorser(req_with_fees_json, e_did)
+        req_with_fees_json = await ledger.multi_sign_request(wallet_handler, author_did, req_with_fees_json)
+        req_with_fees_json = await ledger.multi_sign_request(wallet_handler, e_did, req_with_fees_json)
+        res3 = json.loads(await ledger.submit_request(pool_handler, req_with_fees_json))
+        print(res3)
+        assert res3['op'] == 'REPLY'
