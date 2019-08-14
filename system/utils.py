eacb2d4c882f6d6a4682761ca317eb5c114a751c
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-08-14 18:50:55 +0300

    update
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- system/utils.py
+++ system/utils.py
@@ -98,6 +98,34 @@ ORIGINAL_MAPPING = {
     'i-0d2d372e6a3c5e017': 'vol-0accc7c6548af7a7a'
 }
 
+UPGRADE_MAPPING = {
+    'i-06d864ffc17f20f94': 'vol-051af3baaf0f6cd79',
+    'i-0d38585877dc14755': 'vol-07995a4f913fb48e0',
+    'i-07813055c26ecf5d2': 'vol-04b843e80d43803e2',
+    'i-0ba6de7b7e4ac763a': 'vol-02a0b13192ba4dc24',
+    'i-0f6dedb3ee93e1138': 'vol-07b0f9b5dfc7761cd',
+    'i-043ba82aa40f0fdae': 'vol-0745711426b43482d',
+    'i-0db8a21e6c0252ba7': 'vol-00f187f38a7fea117',
+    'i-06784287da28fa930': 'vol-0a5becbbacaba38fc',
+    'i-0995b5b9f320a2824': 'vol-0bc8402bf3c0716ee',
+    'i-0d1b2f330f139ea85': 'vol-0d29cd9ab1bcb1fe7',
+    'i-0ca3f7bf60d60d133': 'vol-04336c0879d61f142',
+    'i-06836b3ffe6aaca39': 'vol-0642137ca1755c56a',
+    'i-06f30f8aed3af1d4c': 'vol-0732baa1820578a13',
+    'i-058781262b6761fdc': 'vol-0e50222cf92f650ba',
+    'i-042ee39c972737df7': 'vol-04d47d0e36b9d1654',
+    'i-0852c0983a638d6d9': 'vol-094541febda5dd7a6',
+    'i-0aaed544f37ee52c1': 'vol-0fc5c2167d798fb14',
+    'i-0fcb2d529e3f9a04f': 'vol-070f8553ee5916321',
+    'i-0eb04f156b1c51b14': 'vol-0207754c5da19b6a3',
+    'i-07cdbd66e43be4296': 'vol-0bf6bb9e58ee1fccf',
+    'i-03078455ca5dda6b8': 'vol-01335c35c5df2c3ea',
+    'i-00cd5b1dca8a078e1': 'vol-0909aadb945034638',
+    'i-0eb6a38d4fe2d47a5': 'vol-04cb82f1a3cbb7d03',
+    'i-08a30f8a12db050f9': 'vol-07eb8485e8950ce45',
+    'i-0d2d372e6a3c5e017': 'vol-0b33ffe97a96507f8'
+}
+
 
 def run_async_method(method, *args, **kwargs):
     loop = asyncio.get_event_loop()
