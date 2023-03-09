929cfc1ee0eb062f268722ce7cd5bd7eafad6344
pSchlarb <p.schlarb@esatus.com>
2023-03-09 14:55:10 +0000

    parameterized extention GHA
    Signed-off-by: pSchlarb <p.schlarb@esatus.com>

--- system/docker/node/getDeps.sh
+++ system/docker/node/getDeps.sh
@@ -2,7 +2,7 @@ function getDeps(){
     echo "$(apt show ${1} 2>/dev/null  | grep -P "Depends: " | sed 's~Depends:~~g;s~(~~g;s~)~~g;s~>~~g;s~<~~g' |tr -d " \t\n\r" | tr "," "\n")"
 }
 
-aptStr=$(dpkg-deb -I sovrin_*.deb | grep -P "Depends: " | sed 's~Depends:~~g;s~(~~g;s~)~~g;s~>~~g;s~<~~g' | tr -d " \t\n\r" | tr "," "\n")
+aptStr=$(dpkg-deb -I $1 | grep -P "Depends: " | sed 's~Depends:~~g;s~(~~g;s~)~~g;s~>~~g;s~<~~g' | tr -d " \t\n\r" | tr "," "\n")
 for item in $aptStr; do 
     aptStr+="\n$(getDeps ${item})"
 done 
